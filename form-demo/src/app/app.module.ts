127bce9f7639a470b6300c0610d206ae27596ca1
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2021-03-29 09:26:09 +0200

    punto 2

--- form-demo/src/app/app.module.ts
+++ form-demo/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
+
+import {   //Importare le funzionalità nella nostra applicazione
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormDemoComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,  //Anche qui
+    ReactiveFormsModule,
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
